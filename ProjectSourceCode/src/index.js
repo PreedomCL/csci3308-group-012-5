d963cd75e3790848f5e36b3211fe0e1dc390abe0
Curtis Preedom <curtispreedom@gmail.com>
2025-04-16 14:13:00 -0600

    Reference env var for db host and port

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -21,8 +21,8 @@ const fs = require('fs');
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
+  host: process.env.POSTGRES_HOST || 'db', // the database server
+  port: process.env.POSTGRES_PORT || 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
