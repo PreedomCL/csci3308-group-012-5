feee8677b8dffdcf505e1fc4ac21244b65685b61
joehpaulus <joho8439@colorado.edu>
2025-04-17 16:50:02 -0600

    fixed merge conflict and styling is consistent  with the different cards

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -531,7 +531,7 @@ app.get('/profile', async(req, res) => {
     console.log(result);
     res.render('pages/profile', {
       //i think this is where im having trouble reading in
-      name: result.username, bio: result.bio, learningstyle: result.learningstyle, classes: result.classnames, profileimage: result.profileimage, allMatches: allMatches, potentialmatches: potentialmatches
+      userID: result.userid, name: result.username, bio: result.bio, learningstyle: result.learningstyle, classes: result.classnames, profileimage: result.profileimage, allMatches: allMatches, potentialmatches: potentialmatches
     })
   }
   catch(error){
